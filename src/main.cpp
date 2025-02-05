4f24e4a08b5870c42400e50364fbd0aecf46643d
DhruvPotdar <dhruvpotdar29@gmail.com>
2025-02-06 02:15:20 +0530

    added a hollow glass sphere and a transparent substance

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,17 @@
 int main() {
   hittable_list world;
   auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-  auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-  auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-  auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.1);
+  auto material_left = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+  auto material_center = make_shared<dielectric>(1.6);
+  auto material_bubble = make_shared<dielectric>(1.0 / 1.50);
+  auto material_right = make_shared<metal>(color(0.7, 0.7, 0.7), 0.1);
 
   world.add(
       make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-  world.add(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.2, material_center));
-  world.add(make_shared<sphere>(point3(-0.7, 0.0, -1.0), 0.5, material_left));
-  world.add(make_shared<sphere>(point3(0.7, 0.0, -1.0), 0.5, material_right));
+  world.add(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.3, material_center));
+  world.add(make_shared<sphere>(point3(-0.7, 0.0, -1.0), 0.4, material_left));
+  world.add(make_shared<sphere>(point3(-0.0, 0.0, -1.2), 0.2, material_bubble));
+  world.add(make_shared<sphere>(point3(0.7, 0.0, -1.0), 0.4, material_right));
 
   camera cam;
   cam.aspect_ratio = 16.0 / 9.0;
